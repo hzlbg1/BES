5c898c80d0b3f4955805cf48bb6a44710941457e
bbh4897 <42186003+bbh4897@users.noreply.github.com>
2025-09-20 12:29:56 +0300

    created test class

---
+++ src/test/java/com/burcu/bes/BesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.burcu.bes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
