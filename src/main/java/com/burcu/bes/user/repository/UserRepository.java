615c0989f8dcab8224ec52137374eae5e77cbf81
bbh4897 <42186003+bbh4897@users.noreply.github.com>
2025-09-21 10:40:23 +0300

    Revert "test"
    This reverts commit 8502d24a1585f6b7cfc33f0a1aa2d72800a2afc4.

--- src/main/java/com/burcu/bes/user/repository/UserRepository.java
+++ src/main/java/com/burcu/bes/user/repository/UserRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmailIgnoreCase(String email);
-    boolean existsByEmail(String email); //test
+    boolean existsByEmail(String email);
 }
